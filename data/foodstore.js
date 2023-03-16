4beff13b0659e276cc1998f4279cdfe5ab79a6ab
Rapheal Ajiboye <raphoooo95@gmail.com>
2023-03-16 22:07:31 +0100

    fixing foodmenu price

--- data/foodstore.js
+++ data/foodstore.js
@@ -1,8 +1,8 @@
 exports.foodStore = [
-    {id: "1", name: "Ofada rice", price: "$1000"},
-    {id: "2", name: "Burger", price: "$1500"},
-    {id: "3", name: "Chicken", price: "$2000"},
-    {id: "4", name: "Plantain", price: "$1500"},
-    {id: "5", name: "Pounded yam", price: "$500"}
+    {id: "1", name: "Ofada rice", price: "$10"},
+    {id: "2", name: "Burger", price: "$15"},
+    {id: "3", name: "Chicken", price: "$20"},
+    {id: "4", name: "Plantain", price: "$15"},
+    {id: "5", name: "Pounded yam", price: "$10"}
 
 ]
